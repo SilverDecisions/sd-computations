b6314057a734c28811ac81c2ec08fe74fb159467
mwasiluk <m.wasiluk89@gmail.com>
2017-04-19 18:24:37 +0200

    #116 ExtendableError - removed inheritance from Error

--- src/jobs/engine/exceptions/extendable-error.js
+++ src/jobs/engine/exceptions/extendable-error.js
@@ -1,13 +1,8 @@
-export class ExtendableError extends Error {
+export class ExtendableError {
     data;
     constructor(message, data) {
-        super(message);
+        this.message = message;
         this.data = data;
         this.name = this.constructor.name;
-        if (typeof Error.captureStackTrace === 'function') {
-            Error.captureStackTrace(this, this.constructor);
-        } else {
-            this.stack = (new Error(message)).stack;
-        }
     }
 }
